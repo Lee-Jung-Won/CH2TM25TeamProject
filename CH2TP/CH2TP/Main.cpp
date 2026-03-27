30fb3356745055883d717783eaa40efe63884f6c
junahak <junahak0019@gmail.com>
2026-03-27 11:44:55 +0900

    게임 메뉴

--- CH2TP/CH2TP/Main.cpp
+++ CH2TP/CH2TP/Main.cpp
@@ -15,21 +15,19 @@ int main() {
 
     HealthPotion hp(3); // start potion
     player.addhpinventory(&hp);
-    
-    player.ShowStatus();
 
     DrunkenOjisang monster;
 
-    BattleSystem::StartBattle(player);
     while (true)
     {
+        cout << "\033[33m\n========<Game Menu>========\n\t1.Battle\n\t2.Store\n\t0.Exit\033[0m\nSelect a number: ";
         cin >> choice;
         if (choice == 0) break;
         switch (choice)
         {
         case 1: BattleSystem::StartBattle(player); break;
 
-        case 2: cout << "����"; break;
+        case 2: cout << "Store"; break;
         }
     }
 
